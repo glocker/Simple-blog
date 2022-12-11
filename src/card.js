ca528302de977c67ecc2f2aa289a5cfb99fb9cec
George Glocker <georgeglocker@gmail.com>
2022-12-11 23:50:54 +0400

    Closed TODO, getting image by id now

--- src/card.js
+++ src/card.js
@@ -1,7 +1,7 @@
-let url = document.location.href;
-let params = (new URL(url)).searchParams;
-let articleName = params.get("name");
-let articleDate = params.get("date");
+const url = document.location.href;
+const params = (new URL(url)).searchParams;
+const articleName = params.get("name");
+const articleDate = params.get("date");
 
 // DOM elements
 const cardTitle = document.getElementById('cardTitle');
@@ -11,16 +11,19 @@ const cardImage = document.getElementById('image');
 
 // Set clicked article name to title of new card
 cardTitle.innerHTML = articleName;
-
 cardBreadcrumbs.innerHTML = articleName + " (" + articleDate + ")";
 
-// TODO: grab image from imagesList by imageMask only through photoId
-const imageMask = 'https://picsum.photos/id/10/500/400';
+const imageWidth = 500;
+const imageHeight = 400;
+
+function getImage(id) {
+    return `https://picsum.photos/id/${id}/${imageWidth}/${imageHeight}`
+}
 
 const imagesList = {
-    "Der nächste Schritt ist getan: Wir sind klimaneutral": "https://picsum.photos/id/10/500/400",
-    "Endlich wieder INTERSCHUTZ": "https://picsum.photos/id/12/500/400",
-    "Die LPS II im Freizeitgebrauch": "https://picsum.photos/id/11/500/400",
+    "Der nächste Schritt ist getan: Wir sind klimaneutral": getImage(10),
+    "Endlich wieder INTERSCHUTZ": getImage(12),
+    "Die LPS II im Freizeitgebrauch": getImage(11),
 }
 
 // Set clicked article image in new card
